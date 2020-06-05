49df8d5eba4ed56c9d119e7a2c765b1db46547ae
Tianyue Cao <854069858@qq.com>
2020-06-05 18:18:02 +0800

    update ql.py

--- hw4/code/ql.py
+++ hw4/code/ql.py
@@ -5,10 +5,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-epsilons = [0.7]
-alphas = [0.1, 0.3, 0.5, 0.7, 0.9]
+epsilons = [0, 0.1, 0.3, 0.5, 0.7, 0.9]
+alphas = [0.1]
 gammas = [1]
-max_e = 100000
+max_e = 40000
+test_game_num = 100000
 
 states = [d*100+p for d in range(1, 11) for p in range(1, 22)]
 states.append("terminal")
@@ -30,7 +31,7 @@ def train(e, a, g):
 	for i in range(max_e):
 		current_s = init_state()
 
-		if (i+1) % 100 == 0:
+		if (i+1) % 400 == 0:
 			cumr.append(rs)
 			cumes.append(i+1)
 			rs = 0
@@ -91,6 +92,33 @@ def draw_3d():
 	plt.show()
 
 
+def win_rate(game_num):
+	global q_table
+	win, lose, fair = 0, 0, 0
+
+	for i in range(game_num):
+		current_s = init_state()
+
+		while current_s != "terminal":
+			if (random.uniform(0, 1) > e) or (q_table.loc[current_s] == 0).all():
+				current_a = random.choice(actions)
+			else:
+				current_a = q_table.loc[current_s].idxmax()
+			
+			next_s, reward = step(current_s, current_a)
+			current_s = next_s
+
+		if reward == 1.0:
+			win += 1
+		elif reward == -1.0:
+			lose += 1
+		else:
+			fair += 1
+
+	print("total={}, win={}, lose={}, fair={}, win rate={}"
+		.format(game_num, win, lose, fair, win * 1.0 / game_num))
+
+
 if __name__ == '__main__':
 	res = {}
 	for e in epsilons:
@@ -98,6 +126,8 @@ if __name__ == '__main__':
 			for g in gammas:
 				es, vsum, cumes, cumr = train(e, a, g)
 				res[(e, a, g)] = (es, vsum, cumes, cumr)
-	draw_learning_curve(res)
+				print("epsilon={}, alpha={}".format(e, a))
+				win_rate(test_game_num)
+	# draw_learning_curve(res)
 	draw_learning_curve1(res)
 	draw_3d()
